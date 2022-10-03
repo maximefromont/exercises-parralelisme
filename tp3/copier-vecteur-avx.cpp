8a19434998a2e6db5040ce1078c2409f817bcf0e
Maxime Fromont <mfromon@a-999903.ups.u-psud.fr>
2022-10-03 12:30:52 +0200

    aled tp3

--- tp3/copier-vecteur-avx.cpp
+++ tp3/copier-vecteur-avx.cpp
@@ -12,28 +12,34 @@
 #define NREPET 1024
 
 void afficherUsage()
-{ 
+{
   printf("Usage: ./copier-vecteur-avx [taille-du-tableau]\n");
 }
 
 int main(int argc, char **argv)
 {
-  if (argc < 2) { 
+  if (argc < 2) {
     afficherUsage();
     return 1;
   }
   int dim = std::atoi(argv[1]);
 
   // Allouer et initialiser deux tableaux de flottants de taille dim alignes par 32 octets
-  float* tab0;
-  float* tab1;
-  // A FAIRE ...
-  
+  float* tab0 = (float*) malloc(dim*sizeof(float));
+  float* tab1 = (float*) malloc(dim*sizeof(float));
+  for(int i; i<dim; i++)
+  {
+	  tab0[i] = i;
+  }
+
   // Copier tab0 dans tab1 de manière scalaire~(code non-vectorise).
   // On repete NREPET fois pour mieux mesurer le temps d'execution
   auto start = std::chrono::high_resolution_clock::now();
   for (int repet = 0; repet < NREPET; repet++) {
-    // A FAIRE ...
+	for(int i; i<dim; i++)
+    {
+        tab1[i] = tab0[i];
+    }
   }
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diffSeq = end-start;
@@ -42,7 +48,9 @@ int main(int argc, char **argv)
   // Copier tab0 dans tab1 de maniere vectorisee avec AVX
   start = std::chrono::high_resolution_clock::now();
   for (int repet = 0; repet < NREPET; repet++) {
-    // A FAIRE ...
+    __m256 r1;
+    r1 = _mm256_load_ps(tab0);
+    _mm256_store_ps(tab1, r1);
   }
   end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diffPar = end-start;
@@ -53,7 +61,12 @@ int main(int argc, char **argv)
   // Copier tab0 dans tab1 de maniere vectorisee avec AVX et deroulement de facteur 4
   start = std::chrono::high_resolution_clock::now();
   for (int repet = 0; repet < NREPET; repet++) {
-    // A FAIRE ...
+    __m256 r1;
+    r1 = _mm256_load_ps(tab0);
+    r2 = _mm256_load_ps(tab0+1);
+    r3 = _mm256_load_ps(tab0+2);
+    r4 = _mm256_load_ps(tab0+3);
+    _mm256_store_ps(tab1, r1);
   }
   end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diffParDeroule = end-start;
